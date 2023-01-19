495ade4f1b2b651cdb50c8944c61710e1d327dfd
syhzrl <aril@sanspaper.com>
2023-01-19 12:53:03 +0800

    Project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent, useState } from 'react';
+
+const App: FunctionComponent = () => {
+    return (
+        <div>
+            <h1>EYYO</h1>
+        </div>
+    );
+};
+
+export default App;
